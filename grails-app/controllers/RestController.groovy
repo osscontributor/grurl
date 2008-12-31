9bfbb49f50002f096376b36abd70b196f47a3204
rhyolight <rhyolight@gmail.com>
2008-12-31 12:44:56 -0600

    Added rest controller tests and passing code in RestController.

--- grails-app/controllers/RestController.groovy
+++ grails-app/controllers/RestController.groovy
@@ -1,4 +1,26 @@
+import grails.converters.XML
+
 class RestController {
+    
+    def grurlService
 
     def index = { }
+    
+    def generate = {
+        def xmlResult = XML.parse(request)
+        
+        def realUrl = grurlService.refine(xmlResult)      
+        def result 
+        try {
+            result = grurlService.resolve(realUrl)
+        } catch (GrurlException ex) {
+            return render(contentType: "application/xml", encoding: "UTF-8") {
+                error("${ex.message}")
+            }
+        }
+        
+        render(contentType: "application/xml", encoding: "UTF-8") {
+            grurl("$result")
+        }
+    }
 }
--- test/unit/RestControllerTests.groovy
+++ test/unit/RestControllerTests.groovy
@@ -1,13 +1,46 @@
+import grails.test.ControllerUnitTestCase
+
 class RestControllerTests extends ControllerUnitTestCase {
 
-    void testSomething() {
+    void testOneRawUrlSuccess() {
+        def newGrurl = 'http://grurl.com/WORD'
+        
+        def grurlServiceControl = mockFor(GrurlService)
+        grurlServiceControl.demand.refine { url ->
+            url
+        }
+        grurlServiceControl.demand.resolve { url ->
+            newGrurl
+        }
         
         setXmlRequestContent {
             rawUrl('http://www.google.com')
         }
         
         def controller = new RestController()
+        controller.grurlService = grurlServiceControl.createMock()
         controller.generate()
         
+        assertEquals "<grurl>${newGrurl}</grurl>", mockResponse.contentAsString
+    }
+    
+    void testOneRawUrlFailure_ReturnsErrorMessage() {
+        def grurlServiceControl = mockFor(GrurlService)
+        grurlServiceControl.demand.refine { url ->
+            url
+        }
+        grurlServiceControl.demand.resolve { url ->
+            throw new GrurlException('error message')
+        }
+        
+        setXmlRequestContent {
+            rawUrl('!bad url input!')
+        }
+        
+        def controller = new RestController()
+        controller.grurlService = grurlServiceControl.createMock()
+        controller.generate()
+        
+        assertEquals "<error>error message</error>", mockResponse.contentAsString
     }
 }
